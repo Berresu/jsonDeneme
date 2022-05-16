9217d33e45674aa515572563f650eac5973b5017
Berresu <90646787+Berresu@users.noreply.github.com>
2022-05-16 20:16:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-let baglanti = new XMLHttpRequest();
-baglanti.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       console.log(baglanti.responseText);
-    }
-};
-baglanti.open("GET", "data.json", true);
-baglanti.send();
+async function ekranaGetir(){
+    const gelen=await fetch("data.json");
+    let veri=await gelen.json(); 
 
-let ogrenci=JSON.parse(ogrenciler);
-document.getElementById("txtIsim").innerHTML=ogrenci.employees[0].isim;
-document.getElementById("txtSoyisim").innerHTML=ogrenci.employees[0].soyisim;
-document.getElementById("numberNumarasi").innerHTML=ogrenci.employees[0].ogrenciNumarasi;
-document.getElementById("numberYasi").innerHTML=ogrenci.employees[0].yas;
-document.getElementById(txtCinsiyeti).innerHTML=ogrenci.employees[0].cinsiyet;
+    document.getElementById("txtIsim").innerHTML=veri.isim;
+    document.getElementById("txtSoyisim").innerHTML=veri.soyisim;
+    document.getElementById("numberNumarasi").innerHTML=veri.ogrenciNumarasi;
+    document.getElementById("numberYasi").innerHTML=veri.yas;
+    document.getElementById("txtCinsiyeti").innerHTML=veri.cinsiyet;
+}
